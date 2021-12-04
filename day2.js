296350ace679a308b69e963caa1ece4522fa34bf
Stephen Lafortune <slafor24@gmail.com>
2021-12-03 21:52:23 -0500

    day 2

--- day2.js
+++ day2.js
@@ -1,6 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const text = (0, fs_1.readFileSync)("./input.txt", "utf-8");
+const text = (0, fs_1.readFileSync)("./day2PuzzleInput.txt", "utf-8");
 const textByLine = text.split("\n");
-const numberByLine = textByLine.map((x) => parseInt(x));
+var direction;
+(function (direction) {
+    direction["Forward"] = "forward";
+    direction["Down"] = "down";
+    direction["Up"] = "up";
+})(direction || (direction = {}));
+const numberByLine = textByLine.map((x) => {
+    const line = x.split(" ");
+    return { direction: line[0], units: parseInt(line[1]) };
+});
+class Submarine {
+    constructor() {
+        this.horizontalPosition = 0;
+        this.depth = 0;
+        this.aim = 0;
+    }
+    move(moveDirection, units) {
+        if (moveDirection === direction.Up) {
+            this.aim -= units;
+        }
+        if (moveDirection === direction.Down) {
+            this.aim += units;
+        }
+        if (moveDirection === direction.Forward) {
+            this.horizontalPosition += units;
+            this.depth += this.aim * units;
+        }
+    }
+}
+const MySubmarine = new Submarine();
+numberByLine.forEach((x) => {
+    MySubmarine.move(x.direction, x.units);
+});
+console.log(MySubmarine.depth, MySubmarine.horizontalPosition);
--- day2.ts
+++ day2.ts
@@ -1,5 +1,50 @@
 import { readFileSync } from "fs";
 
-const text = readFileSync("./input.txt", "utf-8");
+const text = readFileSync("./day2PuzzleInput.txt", "utf-8");
 const textByLine: string[] = text.split("\n");
-const numberByLine: number[] = textByLine.map((x) => parseInt(x));
+
+enum direction {
+  Forward = "forward",
+  Down = "down",
+  Up = "up",
+}
+
+const numberByLine: { direction: direction; units: number }[] = textByLine.map(
+  (x) => {
+    const line = x.split(" ") as [direction: direction, units: string];
+    return { direction: line[0], units: parseInt(line[1]) };
+  }
+);
+
+class Submarine {
+  horizontalPosition: number;
+  depth: number;
+  aim: number;
+
+  constructor() {
+    this.horizontalPosition = 0;
+    this.depth = 0;
+    this.aim = 0;
+  }
+
+  move(moveDirection: direction, units: number): void {
+    if (moveDirection === direction.Up) {
+      this.aim -= units;
+    }
+    if (moveDirection === direction.Down) {
+      this.aim += units;
+    }
+    if (moveDirection === direction.Forward) {
+      this.horizontalPosition += units;
+      this.depth += this.aim * units;
+    }
+  }
+}
+
+const MySubmarine = new Submarine();
+
+numberByLine.forEach((x) => {
+  MySubmarine.move(x.direction, x.units);
+});
+
+console.log(MySubmarine.depth, MySubmarine.horizontalPosition);
